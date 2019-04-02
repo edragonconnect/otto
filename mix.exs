8d0a182af31e6989c757e4b0ea90c3c09c3ce9d7
250lth <gang250lth@gmail.com>
2019-04-02 14:25:58 +0800

    update to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Otto.MixProject do
   def project do
     [
       app: :otto,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
